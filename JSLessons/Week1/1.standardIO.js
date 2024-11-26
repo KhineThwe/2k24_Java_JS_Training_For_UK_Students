181f6596b866189ebf5442372866230d4e1a1e0c
KhineZarThwe <khinezar70122107@gmail.com>
2024-11-26 21:21:34 +0900

    first commit

---
+++ JSLessons/Week1/1.standardIO.js
@@ -0,0 +1,8 @@
+//Standard Input and Output of JS
+const prompt = require("prompt-sync")();
+
+console.log("Hello World");
+
+const age  = prompt("Enter your age: ");
+
+console.log("Your age :"+age)
